bc045352b0d7c285a9c3d8e3927b0ac3373bfb87
Субботин Виталий Андреевич <vitaliy.subbotin@fsight.ru>
2022-06-28 16:43:01 +0500

    летаем между пермью москвой и питером

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { LatLngExpression } from 'leaflet';
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import DestinationSelector from './components/DestinationSelector';
+import Map from './components/Map';
+import { points } from './constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  const [latlng, setLatlng] = useState<LatLngExpression>(points[Object.keys(points)[0]]);
+
+  const handleChange = useCallback((val: string)=>{
+    setLatlng(points[val]);
+    console.log(val);
+  },[]);
+
+  return <>
+    <DestinationSelector onChange={handleChange}/>
+    <Map latlng={latlng}/>
+  </>
+  ;
 }
 
 export default App;
