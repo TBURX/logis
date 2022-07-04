138e7e6894b5c72305d1b031abacca1501d1aa58
TBURX- <subbot.v@gmail.com>
2022-07-04 05:21:23 +0500

    ограничение по размерам для сплиттера

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Map from './components/Map';
 import OrdersPanel from './components/OrdersPanel';
 
 const App: React.FC = () => (
-  <SplitterLayout>
+  <SplitterLayout primaryMinSize={200} secondaryMinSize={500}>
     <OrdersPanel />
     <Map />
   </SplitterLayout>
